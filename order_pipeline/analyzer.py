ceaa1e877a221d2c1aa235f602bb253800015545
king-obobo <oboboebuka1@gmail.com>
2025-10-24 23:37:07 +0100

    Added logic for my Analyzer

--- order_pipeline/analyzer.py
+++ order_pipeline/analyzer.py
@@ -2,3 +2,49 @@
 # Total revenue
 # Average revenue
 # total number of paid/pending/refunded each
+
+class Analyzer:
+    
+    def __init__(self, file: list):
+        self.file = file
+        
+
+    def compute_revenue_vals(self) -> int | float:
+        # Get all the revenue and store in a list
+        # Calculate the total from here
+        all_totals = []
+        for row in self.file:
+            all_totals.append(row["total"])
+        
+        return sum(all_totals), len(all_totals)
+    
+    def compute_total_revenue(self):
+        total_revenue, _ = self.compute_revenue_vals()
+        return round(total_revenue, 2)
+    
+    
+    def compute_average_revenue(self) -> int | float:
+        # Get all the revenue and store in a list
+        # Calculate the total from here
+        total_revenue, no_of_revenue_value = self.compute_revenue_vals()
+        return round(total_revenue / no_of_revenue_value, 2)
+    
+    
+    def compute_payment_status(self) -> dict:
+        paid = 0
+        pending = 0
+        refunded = 0
+        
+        for row in self.file:
+            if row["payment_status"] == "paid":
+                paid+=1
+            if row["payment_status"] == "pending":
+                pending+=1
+            if row["payment_status"] == "refunded":
+                refunded+=1
+                
+        return {
+            "paid": paid,
+            "pending": pending,
+            "refunded": refunded
+        }
--- order_pipeline/pipeline.py
+++ order_pipeline/pipeline.py
@@ -3,6 +3,7 @@
 from reader import ReadJson
 from validator import Validator
 from transformer import Transformer
+from analyzer import Analyzer
 
 my_file = ReadJson("shoplink.json").read_json_file()
 validated_file = Validator(my_file).validate_file()
@@ -15,6 +16,13 @@ transformed_data = Transformer(validated_file).transform_data()
 # for row in validator.skipped_rows:
 #     print(row)
 
-for row in transformed_data:
-    print(row)
+# for row in transformed_data:
+#     print(row)
 
+my_analyzer = Analyzer(transformed_data)
+total_revenue = my_analyzer.compute_total_revenue()
+print(total_revenue)
+average_revenue = my_analyzer.compute_average_revenue()
+print(average_revenue)
+payment_status = my_analyzer.compute_payment_status()
+print(payment_status)
