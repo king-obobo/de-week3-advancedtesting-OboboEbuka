0ae4e231910be2da57e27c7933de66aae262382d
king-obobo <oboboebuka1@gmail.com>
2025-10-26 14:30:04 +0100

    Added a pipeline class and completed the Logic for it

---
+++ order_pipeline/main.py
@@ -0,0 +1,22 @@
+from pipeline import Pipeline
+
+
+if __name__ == "__main__":
+    pipeline = Pipeline("shoplink.json")
+    pipeline.run()
+    pipeline.print_summary_statistics()
+
+    print("\n============= PRINTING VALIDATED DATA=================\n")
+    validate_data = pipeline.get_validated_data()
+    for row in validate_data:
+        print(row)
+        
+    print("\n============= PRINTING TRANSFORMED DATA=================\n")
+    transformed_data = pipeline.get_transformed_data()
+    for row in transformed_data:
+        print(row)
+        
+    print("\n============= SKIPPED ROWS=================\n")
+    skipped_data = pipeline.get_skipped_rows()
+    for row in skipped_data:
+        print(row)
--- order_pipeline/pipeline.py
+++ order_pipeline/pipeline.py
@@ -8,42 +8,70 @@ from exporter import Exporter
 from datetime import datetime
 
 
-if __name__ == "__main__":
-
-    my_file = ReadJson("shoplink.json").read_json_file()
-    validator = Validator(my_file)
-    validated_file = validator.validate_file()
-    transformed_data = Transformer(validated_file).transform_data()
-    # print(type(validated_file))
-    for row in validated_file:
-        print(row)
+class Pipeline:
+    
+    def __init__(self, json_file):
+        self.json_file = json_file
         
-    print("\nSKIPPED ROWS")
-    for row in validator.skipped_rows:
-        print(row)
-
-    print("\nVALIDATED DATA")
-    for row in transformed_data:
-        print(row)
-
-    my_analyzer = Analyzer(transformed_data)
-    total_revenue = my_analyzer.compute_total_revenue()
-    print(f"\nThe Total Revenue generated is ${total_revenue}")
-    average_revenue = my_analyzer.compute_average_revenue()
-    print(f"The Average Revenue generated is ${average_revenue}\n")
+        
+    def run(self):
+        # Read In the File
+        READER = ReadJson(self.json_file)
+        try:
+            self.valid_json_file = READER.read_json_file()
+            
+            # Validate the File
+            self.VALIDATOR = Validator(self.valid_json_file)
+            self.validated_data = self.VALIDATOR.validate_file()
+            
+            # Transform Validated Data
+            TRANSFORMER = Transformer(self.validated_data)
+            self.transformed_data = TRANSFORMER.transform_data()
+            
+            # Run my Analysis
+            self.ANALYZER = Analyzer(self.transformed_data)
+            
+        except Exception as e:
+            print(f"There is something wrong with the Json file: {e}")
+            
+        # Export Data
+        try:
+            EXPORTER = Exporter(self.transformed_data)
+            EXPORTER.export_to_json()
+        except Exception as e:
+            print("Something went wrong while exporting the File")
+            
+        print("===========PIPELINE RAN SUCCESSFULLY===============")
+        
+        
+    def print_summary_statistics(self) -> None:
+        
+        # Prints the total Revenue
+        print(f"\nThe Total Revenue generated is ${self.ANALYZER.compute_total_revenue()}")
+        
+        # Average Revenue
+        print(f"The Average Revenue generated is ${self.ANALYZER.compute_average_revenue()}\n")
+        
+        print("\n====================SUMMARY STATISTICS FOR PAYMENT STATUS=========================")
+        payment_status = self.ANALYZER.compute_payment_status()
+        print(f"Total Paid payment status: {payment_status['paid']}")
+        print(f"Total Pending payment status: {payment_status['pending']}")
+        print(f"Total Refunded payment status: {payment_status['refunded']}")
+        print("\n====================END OF SUMMARY STATISTICS FOR PAYMENT STATUS=========================\n")
+        
+        return None
     
-    print("\n====================SUMMARY STATISTICS FOR PAYMENT STATUS=========================")
-    payment_status = my_analyzer.compute_payment_status()
-    print(f"Total Paid payment status: {payment_status['paid']}")
-    print(f"Total Pending payment status: {payment_status['pending']}")
-    print(f"Total Refunded payment status: {payment_status['refunded']}")
-    print("\n====================END OF SUMMARY STATISTICS FOR PAYMENT STATUS=========================\n")
     
-    # bad_data = [
-    #     {"not_ok": {1, 2, 3}},           # set
-    #     {"not_ok": datetime.now()},      # datetime
-    #     {"not_ok": b"bytes"}]           # bytes
-    # test_data = [123]
-    # Exporter(bad_data).export_to_json()
-
-    Exporter(transformed_data).export_to_json()
+    # To see the validated Data
+    def get_validated_data(self):
+        return self.validated_data
+    
+    
+    # To see the transformed Data
+    def get_transformed_data(self):
+        return self.transformed_data
+    
+    
+    # To see the skipped row
+    def get_skipped_rows(self):
+        return self.VALIDATOR.skipped_rows
